ef788115e0f693426c85d473ddc3228ecde95ce4
Hasibe <hasibe.zafer@ogr.iu.edu.tr>
2022-12-14 19:55:19 +0300

    added icon path util

---
+++ lib/utilities/icon_path_util.dart
@@ -0,0 +1,6 @@
+
+//Bottom Navigation Bar Icon Path
+const String kIconPathBottomNavigationBarHome = 'assets/icons/ic_bottom_navigation_bar_home.png';
+const String kIconPathBottomNavigationBarAnimals = 'assets/icons/ic_bottom_navigation_bar_animals.png';
+const String kIconPathBottomNavigationBarPlants = 'assets/icons/ic_bottom_navigation_bar_plants.png';
+const String kIconPathBottomNavigationBarProfile = 'assets/icons/ic_bottom_navigation_bar_profile.png';

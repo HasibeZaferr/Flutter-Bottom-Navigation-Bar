5dc19cf38e03c9e2bb7bdc11641406bbf7608921
Hasibe <hasibe.zafer@ogr.iu.edu.tr>
2022-12-14 21:52:15 +0300

    added animals screen

--- lib/screens/animals_screen.dart
+++ lib/screens/animals_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../utilities/icon_path_util.dart';
+
 class AnimalsScreen extends StatelessWidget {
   const AnimalsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: const Text('Animals'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0.2,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Image(
+                image: AssetImage(kIconPathBottomNavigationBarAnimals),
+                height: 100.0),
+            SizedBox(height: 50.0),
+            Text(
+              'My Sweet Home',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

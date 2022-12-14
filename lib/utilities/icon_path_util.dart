e775231859856f69bc625799cdd93ad29a9baf3a
Hasibe <hasibe.zafer@ogr.iu.edu.tr>
2022-12-14 20:23:26 +0300

    added deactive icons

--- lib/utilities/icon_path_util.dart
+++ lib/utilities/icon_path_util.dart
@@ -1,6 +1,19 @@
+//Bottom Navigation Bar Active Icon Path
+const String kIconPathBottomNavigationBarHome =
+    'assets/icons/ic_bottom_navigation_bar_home.png';
+const String kIconPathBottomNavigationBarAnimals =
+    'assets/icons/ic_bottom_navigation_bar_animals.png';
+const String kIconPathBottomNavigationBarPlants =
+    'assets/icons/ic_bottom_navigation_bar_plants.png';
+const String kIconPathBottomNavigationBarProfile =
+    'assets/icons/ic_bottom_navigation_bar_profile.png';
 
-//Bottom Navigation Bar Icon Path
-const String kIconPathBottomNavigationBarHome = 'assets/icons/ic_bottom_navigation_bar_home.png';
-const String kIconPathBottomNavigationBarAnimals = 'assets/icons/ic_bottom_navigation_bar_animals.png';
-const String kIconPathBottomNavigationBarPlants = 'assets/icons/ic_bottom_navigation_bar_plants.png';
-const String kIconPathBottomNavigationBarProfile = 'assets/icons/ic_bottom_navigation_bar_profile.png';
+//Bottom Navigation Bar Deactive Icon Path
+const String kIconPathBottomNavigationBarHomeDeactive =
+    'assets/icons/ic_bottom_navigation_bar_home_deactive.png';
+const String kIconPathBottomNavigationBarAnimalsDeactive =
+    'assets/icons/ic_bottom_navigation_bar_animals_deactive.png';
+const String kIconPathBottomNavigationBarPlantsDeactive =
+    'assets/icons/ic_bottom_navigation_bar_plants_deactive.png';
+const String kIconPathBottomNavigationBarProfileDeactive =
+    'assets/icons/ic_bottom_navigation_bar_profile_deactive.png';
